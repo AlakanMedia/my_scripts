3819d7c2342e86d001ce379ad34468f4588c58c3
AlakanMedia <alakan439@gmail.com>
2024-04-08 08:18:53 -0500

    I modified the bash script to check if a usb flash drive is connected

--- bash/images.sh
+++ bash/images.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/bash
 
-CHOSEN=`ls "$HOME"/images | dmenu -p 'Directorys' -i`
+OPTIONS=`ls "$HOME"/images/`
+USB_WALLPAPERS='/mnt/usb/wallpapers/'
 
-if [ -z "$CHOSEN" ]; then
-    exit 1
+if [ -d "$USB_WALLPAPERS" ]; then
+    OPTIONS="$OPTIONS\nusb"
 fi
 
-nsxiv "$HOME"/images/"$CHOSEN"
+CHOSEN=`echo -e "$OPTIONS" | dmenu -p 'Directories' -i`
+
+if [ -z "$CHOSEN" ]; then
+    exit 0
+fi
+
+if [ "$CHOSEN" = "usb" ]; then
+    nsxiv "$USB_WALLPAPERS"
+else
+    nsxiv "$HOME"/images/"$CHOSEN"
+fi
 
 exit 0
